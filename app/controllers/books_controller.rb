3c8ae910faa2285460f71d2da3b8c345d8f1dbb7
Eugene <eugeneayonga@gmail.com>
2022-10-10 15:18:01 +0300

    Added a show method in the Books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,4 +4,9 @@ class BooksController < ApplicationController
         book = Book.all
         render json: book, status: :ok
     end
+
+    def show
+        book = Book.find_by(id: params[:id])
+        render json: book, status: :ok
+    end
 end
