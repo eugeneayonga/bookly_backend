4222b47e91baac5b9b11dea1e087e630709537cf
Eugene <eugeneayonga@gmail.com>
2022-10-10 17:51:46 +0300

    Built App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import "./App.css";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import Books from "./Books";
+import FormPage from "./FormPage";
+import { Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch(`http://localhost:9292/books`)
+      .then((response) => response.json())
+      .then((data) => {
+        setBooks(data);
+      });
+  }, []);
+
+
+  function addBooks(newBook) {
+    const updatedBooks = [newBook, ...books];
+    setBooks(updatedBooks);
+  }
+
+
+  function addReview(newReview) {
+    const bookToUpdate = books.find((book) => {
+      return book.id === newReview.book_id;
+    });
+
+    const updatedReviews = [newReview, ...bookToUpdate.reviews];
+    bookToUpdate.reviews = updatedReviews;
+    setBooks(
+      books.map((book) => (book.id === bookToUpdate.id ? bookToUpdate : book))
+    );
+
+    setReviews(updatedReviews);
+  }
+
+
+  function handleUpdateLikes(updateLikes) {
+    const updatedLikes = books.map((book) => {
+      return book.id === updateLikes.id ? updateLikes : book;
+    });
+    setBooks(updatedLikes);
+  }
+
+
+  function handleDeleteReview(deletedReview) {
+    const bookToUpdate = books.find((book) => {
+      return book.id === deletedReview.book_id;
+    });
+
+    const updatedReview = bookToUpdate.reviews.filter((review) => {
+      return review.id !== deletedReview.id;
+    });
+
+    bookToUpdate.reviews = updatedReview;
+    setBooks(
+      books.map((book) => (book.id === bookToUpdate.id ? bookToUpdate : book))
+    );
+    setReviews(updatedReview);
+  }
+
+
+  const booksToDisplay = books.filter((book) => {
+    return book.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <br />
+      <Route path="/books">
+        <Books
+          handleUpdateLikes={handleUpdateLikes}
+          addReview={addReview}
+          books={books}
+          booksToDisplay={booksToDisplay}
+          searchTerm={searchTerm}
+          onSearch={setSearchTerm}
+          handleDeleteReview={handleDeleteReview}
+          reviews={reviews}
+        />
+      </Route>
+      <Route path="/formpage">
+        <FormPage addBooks={addBooks} />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
     </div>
   );
 }
+
 
 export default App;
