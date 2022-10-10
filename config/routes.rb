76d9dcb396f14be53bf0d6ac98b16a0b79f600fe
Eugene <eugeneayonga@gmail.com>
2022-10-10 15:22:12 +0300

    Added POST /books route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get "/books", to: "books#index"
 
   get "/books/:id", to: "books#show"
+
+  post "/books", to: "books#create"
+  
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
