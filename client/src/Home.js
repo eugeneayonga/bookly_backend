5376d56f4ec80ae01c2d078d213b7a441f081dae
Eugene <eugeneayonga@gmail.com>
2022-10-10 17:39:21 +0300

    Buillt Home.js component

---
+++ client/src/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <h1>Bookly</h1>
+        <p>
+          Welcome to Bookly! Your one stop catalogue for awesome books.
+        </p>
+        <img
+          alt="Attic Bookshelf"
+          src="https://papik.pro/en/uploads/posts/2022-07/1658717710_19-papik-pro-p-background-drawings-library-19.jpg"
+        />
+      </div>
+    </>
+  );
+};
+
+
+export default Home;
